97a2f71b724919eb4f7d22f87f4cab0e429f6a67
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-09-27 04:22:54 +0200

    Add content to About component

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,24 @@
+import Picture from "../assets/images/Dakouri.png";
+
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section className="about" id="about">
+      <div className="about__text">
+        <h2>About me</h2>
+        <p>
+          Hi! I’m Dakouri, a frontend developer. My years of bioscience research
+          skills help me approach software development with strict methodology
+          while being open to creativity.
+        </p>
+        <p>
+          I specialize in React and TypeScript and my motto is “clean code”.
+          With this website, I showcase my work.
+        </p>
+      </div>
+
+      <div className="about__image">
+        <img src={Picture} alt="Dakouri" />
+      </div>
+    </section>
   );
 }
