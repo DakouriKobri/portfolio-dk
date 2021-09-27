3cc2164191cb7491cbaad0816574671f1c0a1d2d
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-09-27 02:43:21 +0200

    Add general portfolio folder sturture

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Kobri</h1>
+      DBECF1 859FD0 2B6CC1 0C2A83 041144
+      <br />
+      041144 0C3198 2B6CC1 CADEE6 859CC6
+    </div>
+  );
+}
+
+export default App;
