d96680c269079424f7eaa650744b061c84b61471
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-09-28 08:58:55 +0200

    Add 'Coming Soon' effect, however, CSS noClick event not workiing yet and need fixing

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,7 +2,12 @@ export default function ProjectCard({ project, onClick }) {
   const { thumbnail, title, isCompleted } = project;
   return (
     <article className="card">
-      <div className="thumbnail-container" onClick={onClick}>
+      <div
+        className={
+          isCompleted ? "thumbnail-container" : "thumbnail-container active"
+        }
+        onClick={onClick}
+      >
         <label className="project-status">COMING SOON</label>
         <img className="thumbnail" src={thumbnail} alt={title} />
       </div>
